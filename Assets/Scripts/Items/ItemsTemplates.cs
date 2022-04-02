30ceeafd284bc2ae9d20080ceaec983479c12ff8
aaraujof <100710936+aaraujof@users.noreply.github.com>
2022-04-02 18:23:09 +0200

    Last commit before pac2

--- Assets/Scripts/Items/ItemsTemplates.cs
+++ Assets/Scripts/Items/ItemsTemplates.cs
@@ -20,7 +20,7 @@ public class ItemsTemplates : MonoBehaviour
 
     public void SpawnItems(List<Vector2> roomsSpawned)
     {
-        int nextItem = 8;
+        int nextItem = 7;
 
         int count = items.Count;
         for (int i = 1; i < roomsSpawned.Count - 1; i++)
@@ -32,7 +32,7 @@ public class ItemsTemplates : MonoBehaviour
                     int randomItem = Random.Range(0, items.Count);
                     Instantiate(items[randomItem], roomsSpawned[i], items[randomItem].transform.rotation);
                     items.Remove(items[randomItem]);
-                    nextItem = nextItem + 5;
+                    nextItem = nextItem + 7;
                     count--;
                 }
             }
