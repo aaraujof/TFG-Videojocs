b2e64f6d56dc099e5b94421e44754ca0dbaca089
aaraujof <100710936+aaraujof@users.noreply.github.com>
2022-04-18 12:03:36 +0200

    Controls
    Add Controls to Main menu.

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject controls;
+    public GameObject playButton;
+    public GameObject controlsButton;
+    public GameObject quitButton;
+    public GameObject title;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
@@ -13,5 +19,25 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void SeeControls()
+    {
+        if (controls.activeSelf)
+        {
+            controls.SetActive(false);
+            title.SetActive(true);
+            playButton.SetActive(true);
+            controlsButton.SetActive(true);
+            quitButton.SetActive(true);
+        }
+        else
+        {
+            controls.SetActive(true);
+            title.SetActive(false);
+            playButton.SetActive(false);
+            controlsButton.SetActive(false);
+            quitButton.SetActive(false);
+        }
     }
 }
